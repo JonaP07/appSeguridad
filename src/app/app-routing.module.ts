e97ea49ff9eeefa7e256911781b99ca12902532f
Jonas07 <jnthnpadillaleon072@gmial.com>
2024-12-04 22:11:11 -0400

    Agregando el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },  
+  { path: 'about', component: AboutComponent },   
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' }, // Redirige a Inicio por defecto
+  { path: '**', redirectTo: '/inicio' } // Redirige rutas no encontradas a Inicio
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
